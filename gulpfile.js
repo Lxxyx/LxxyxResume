64a58be53d10e1ef6427dce190ff87b0dde8d1bd
Lxxyx <Lxxyxzj@gmail.com>
2018-03-16 21:23:15 +0800

    feat: 防止报错

--- gulpfile.js
+++ gulpfile.js
@@ -9,46 +9,55 @@ const fs = require('fs')
 const connect = require('gulp-connect')
 const generatePdf = require('./generate_pdf')
 
-gulp.task('resume-sass', function () {
-  gulp.src('src/scss/resume.scss')
+gulp.task('resume-sass', () => {
+  gulp
+    .src('src/scss/resume.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 4 versions'],
-      cascade: false
-    }))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 4 versions'],
+        cascade: false
+      })
+    )
     .pipe(gulp.dest('dist/css/'))
     .pipe(connect.reload())
 })
 
-gulp.task('icon-sass', function () {
-  gulp.src('src/scss/iconfont.scss')
+gulp.task('icon-sass', () => {
+  gulp
+    .src('src/scss/iconfont.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 4 versions'],
-      cascade: false
-    }))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 4 versions'],
+        cascade: false
+      })
+    )
     .pipe(gulp.dest('dist/iconfont/'))
     .pipe(connect.reload())
 })
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', () => {
   gulp.watch('./src/scss/resume.scss', ['resume-sass'])
   gulp.watch('./src/scss/iconfont.scss', ['icon-sass'])
   gulp.watch('./src/scss/components/*.scss', ['resume-sass'])
 })
 
-gulp.task('json2jade', function () {
-  var info = JSON.parse(fs.readFileSync('./info.json', 'utf-8'))
-  var locals = highlight(info)
-  gulp.src('./src/jade/index.jade')
-    .pipe(jade({
-      locals: locals
-    }))
+gulp.task('json2jade', () => {
+  const info = JSON.parse(fs.readFileSync('./info.json', 'utf-8'))
+  const locals = highlight(info)
+  gulp
+    .src('./src/jade/index.jade')
+    .pipe(
+      jade({
+        locals
+      })
+    )
     .pipe(gulp.dest('./dist/'))
     .pipe(connect.reload())
 })
 
-gulp.task('json2jade:watch', function () {
+gulp.task('json2jade:watch', () => {
   gulp.watch('./info.json', ['json2jade'])
 })
 
@@ -58,7 +67,7 @@ function src2dist(dir) {
 
 function highlight(locals) {
   var locals = JSON.stringify(locals)
-  var re = /`(.+?)`/g
+  const re = /`(.+?)`/g
   locals = locals.replace(re, '<strong>$1</strong>')
   return JSON.parse(locals)
 }
@@ -74,19 +83,27 @@ gulp.task('clean', () => {
   rimrafPromise('./dist/')
 })
 
-gulp.task('deploy', function () {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages({
+gulp.task('deploy', () =>
+  gulp.src('./dist/**/*').pipe(
+    ghPages({
       remoteUrl: 'git@github.com:Lxxyx/lxxyx.github.io.git',
       branch: 'master'
-    }))
+    })
+  )
+)
+
+let port = 9000
+
+// 避免打印时，同时运行开发服务报错
+gulp.task('set-pdf-port', () => {
+  port = 9001
 })
 
-gulp.task('webserver', function () {
+gulp.task('webserver', () => {
   connect.server({
     root: './dist',
     livereload: true,
-    port: 9000
+    port
   })
 })
 
@@ -94,8 +111,8 @@ gulp.task('dev', ['default', 'json2jade:watch', 'sass:watch', 'webserver'])
 
 gulp.task('default', ['icon-sass', 'resume-sass', 'json2jade', 'copy'])
 
-gulp.task('pdf', ['default', 'webserver'], async () => {
-  await generatePdf('http://localhost:9000')
+gulp.task('pdf', ['set-pdf-port', 'default', 'webserver'], async () => {
+  await generatePdf('http://localhost:9001')
   connect.serverClose()
   process.exit(0)
 })
